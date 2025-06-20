fd412b62727abff83bd75317f7ac0089bd57c484
MokshPShah <shahmoksh90651@gmail.com>
2025-06-20 22:34:23 +0530

    v1.3

--- server/createAdmin.js
+++ server/createAdmin.js
@@ -6,8 +6,8 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 mongoose.connect(process.env.MONGO_URI).then(async () => {
-  const hashed = await bcrypt.hash('Moksh9)8(7+', 10) // you can change the password
-  await Admin.create({ username: 'Moksh', password: hashed })
+  const hashed = await bcrypt.hash('admin123', 10) // you can change the password
+  await Admin.create({ username: 'admin', password: hashed })
   console.log('✅ Admin created')
   process.exit()
 })
